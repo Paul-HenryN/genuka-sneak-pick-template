2ea0df164eeeca8335d4c3cf858acd1ec012fdfb
Paul-HenryN <paul-henry.ngankam@2025.ucac-icam.com>
2022-08-28 14:23:51 +0100

    slick carousel integration

--- docs/index.279906cf.js
+++ docs/index.279906cf.js
@@ -1,26 +1,12 @@
-const carousel = document.querySelector(".carousel");
-const carouselItems = carousel.querySelectorAll(".carousel__item");
-let counter = 0;
-let activeItem = carouselItems[counter];
-for (const item of carouselItems)item.style.opacity = 0;
-activeItem.style.opacity = 1;
-setInterval(()=>{
-    counter = counter === carouselItems.length - 1 ? 0 : counter + 1;
-    console.log(counter);
-    let nextItem = carouselItems[counter];
-    activeItem.classList.add("animate__animated");
-    activeItem.classList.add("animate__fadeOut");
-    nextItem.classList.add("animate__animated");
-    nextItem.classList.add("animate__fadeIn");
-    nextItem.onanimationend = ()=>{
-        activeItem.style.opacity = 0;
-        nextItem.style.opacity = 1;
-        activeItem.classList.remove("animate__animated");
-        activeItem.classList.remove("animate__fadeOut");
-        nextItem.classList.remove("animate__animated");
-        nextItem.classList.remove("animate__fadeIn");
-        activeItem = nextItem;
-    };
-}, 5000);
+$(document).ready(function() {
+    $(".carousel__items").slick({
+        prevArrow: $(".carousel__btn_left"),
+        nextArrow: $(".carousel__btn_right"),
+        infinite: true,
+        slidesToScroll: 1,
+        variableHeight: true,
+        variableWidth: true
+    });
+});
 
 //# sourceMappingURL=index.279906cf.js.map
---
+++ src/js/carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".carousel__items").slick({
+    prevArrow: $(".carousel__btn_left"),
+    nextArrow: $(".carousel__btn_right"),
+    infinite: true,
+    slidesToScroll: 1,
+    variableHeight: true,
+    variableWidth: true,
+  });
+});
