f15b9e1b0fe25dcea6cb8c744c9a362192f9ccaa
Paul-HenryN <paul-henry.ngankam@2025.ucac-icam.com>
2022-08-29 12:31:04 +0100

    carousel scroll enter animations

--- docs/index.279906cf.js
+++ docs/index.279906cf.js
@@ -10,8 +10,29 @@ $(document).ready(function() {
     $(".brands__carousel").children(".carousel__items").slick({
         arrows: false,
         infinite: true,
-        variableWidth: true
+        variableWidth: true,
+        variableHeight: true
     });
+    const carouselCards = $(".carousel__card");
+    for (const card of carouselCards){
+        let index = parseInt(card.getAttribute("data-slick-index"));
+        card.style.animationDelay = `${index * 200}ms`;
+        console.log(card.style.animationDelay);
+        card.classList.add("animate__animated");
+    }
+    const controller = new ScrollMagic.Controller();
+    new ScrollMagic.Scene({
+        triggerElement: "#new-arrivals",
+        triggerHook: "#OnEnter",
+        offset: -50
+    }).addTo(controller) // assign the scene to the controller
+    .addIndicators().setClassToggle("#new-arrivals .carousel__card", "animate__fadeInUp");
+    new ScrollMagic.Scene({
+        triggerElement: "#brands",
+        triggerHook: "#OnEnter",
+        offset: -50
+    }).addTo(controller) // assign the scene to the controller
+    .addIndicators().setClassToggle("#brands .carousel__card", "animate__fadeInUp");
 });
 
 //# sourceMappingURL=index.279906cf.js.map
